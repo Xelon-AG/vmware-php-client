0ca4d064f83cb59d16afd9572e10fbdeb310be07
gazhur94 <gazhur94@users.noreply.github.com>
2022-10-24 09:29:22 +0000

    Fix styling

--- src/Data/SoapData.php
+++ src/Data/SoapData.php
@@ -207,7 +207,7 @@ class SoapData
                 'subnetMask' => $subnetMask,
                 'dnsServerList' => $dnsServerList,
                 'gateway' => $gateway,
-            ])
+            ]),
         ]);
     }
 
