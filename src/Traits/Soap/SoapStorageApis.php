c707f6e338a866e793569f4c914052a25af3671b
gazhur94 <gazhur94@users.noreply.github.com>
2022-11-23 09:56:27 +0000

    Fix styling

--- src/Traits/Soap/SoapStorageApis.php
+++ src/Traits/Soap/SoapStorageApis.php
@@ -64,7 +64,7 @@ trait SoapStorageApis
                         'type' => 'Datastore',
                         '_' => $datastore,
                     ],
-                    'provisioningType' => 'thin'
+                    'provisioningType' => 'thin',
                 ]),
                 'capacityInMB' => $capacityInMB,
             ],
