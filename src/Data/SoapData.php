32ee0c85c55b612100cd9eb22740a66e0c1085bf
Andrii Hazhur <a.hazhur@bitcat.agency>
2023-01-31 16:35:04 +0200

    fix: added shares to ps

--- src/Data/SoapData.php
+++ src/Data/SoapData.php
@@ -106,6 +106,7 @@ class SoapData
             'backing' => new VirtualDiskFlatVer2BackingInfo([
                 'fileName' => $blockStoragePath,
                 'diskMode' => 'independent_persistent',
+                'sharing' => 'sharingMultiWriter'
             ]),
             'controllerKey' => $controllerKey,
             'unitNumber' => -1,
