5763c3542d1c0dfb57ac1ae16a9a03bbb0a2e674
Dmitry Filatov <d.filatov@xelon.ch>
2023-06-20 14:49:06 +0300

    fix: remove broadcast

--- src/Events/RequestEvent.php
+++ src/Events/RequestEvent.php
@@ -2,15 +2,12 @@
 
 namespace Xelon\VmWareClient\Events;
 
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class RequestEvent
 {
     use Dispatchable;
-    use InteractsWithSockets;
     use SerializesModels;
 
     public string $requestBody;
@@ -24,10 +21,5 @@ class RequestEvent
         $this->requestBody = $requestBody;
         $this->responseBody = $responseBody;
         $this->status = $status ? 'Success' : 'Error';
-    }
-
-    public function broadcastOn()
-    {
-        return new PrivateChannel('vmware-client');
     }
 }
